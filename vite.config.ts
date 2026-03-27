246d60e14d55bc0593ef5a438385407ce7f8df31
rvnd-sys16 <ramadhanervien@gmail.com>
2026-03-28 00:50:25 +0700

    Enable prerendering in Vike plugin configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import react from "@vitejs/plugin-react";
-import vike from "vike/plugin";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import vike from 'vike/plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [vike(), react()],
+  plugins: [
+    react(),
+    vike({
+      prerender: true // <--- WAJIB TAMBAHKAN INI
+    })
+  ]
 });
